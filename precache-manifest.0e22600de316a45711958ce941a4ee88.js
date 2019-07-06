ce85b3a2b23bdf7ee62d56bb5d53680e2803c9f4
moudev <rigobertomauriciomartinez@gmail.com>
2019-07-06 10:04:24 -0600

    Updates

---
+++ precache-manifest.0e22600de316a45711958ce941a4ee88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223da982b5789511af72af314d50a595",
+    "url": "/subtitles-like-udemy/index.html"
+  },
+  {
+    "revision": "e343ce074cd343cd713a",
+    "url": "/subtitles-like-udemy/static/css/main.3494b8ca.chunk.css"
+  },
+  {
+    "revision": "718803986ae2095a9af1",
+    "url": "/subtitles-like-udemy/static/js/2.bbe72b70.chunk.js"
+  },
+  {
+    "revision": "e343ce074cd343cd713a",
+    "url": "/subtitles-like-udemy/static/js/main.96298df0.chunk.js"
+  },
+  {
+    "revision": "a28a8af6958c4824f3d7",
+    "url": "/subtitles-like-udemy/static/js/runtime~main.cca06a51.js"
+  }
+]);
